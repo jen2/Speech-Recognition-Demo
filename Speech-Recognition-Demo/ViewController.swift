a8ae0e195da18433da9528e03bb79808c17eb5af
jen2 <sipila.jen@gmail.com>
2017-03-02 16:47:14 -0500

    Initial Commit

---
+++ Speech-Recognition-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Speech-Recognition-Demo
+//
+//  Created by Jennifer A Sipila on 3/2/17.
+//  Copyright © 2017 Jennifer A Sipila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
